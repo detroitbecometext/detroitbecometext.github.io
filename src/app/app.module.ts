5e77d9af8dda967419c8dc87f9d203e8eae30003
detroitbecometext <keziabaj@gmail.com>
2018-10-21 16:28:25 +0200

    Finished contact page and fixed chapter 32.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule }    from '@angular/forms';
+import { ReactiveFormsModule }    from '@angular/forms';
 import { SafeHtmlPipe } from './models/SafeHtmlPipe';
 
 import { AppComponent } from './app.component';
@@ -28,7 +28,7 @@ import { ContactComponent } from './components/contact/contact.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -7,22 +9,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  message: string;
-  isDisabled: boolean;
+  language: string;
+  contactForm: FormGroup;
+  isSubmitted: boolean;
+  isSuccess: boolean;
+  isError: boolean;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() 
   {
-    this.isDisabled = true;
+    this.language = this.getLanguage(navigator.language);
+    this.isSubmitted = false;
+    this.isSuccess = false;
+    this.isError = false;
+
+    //Create the control group
+    this.contactForm = new FormGroup({
+      email: new FormControl(''),
+      name: new FormControl(''),
+      message: new FormControl('', Validators.required)
+    });
   }
 
-  onMessageChanged(newValue: string){
-    if(newValue != null && !(newValue === "")){
-      this.isDisabled = false;
+  /**
+   * This function will return a language supported by formspree depending on the user's language.
+   * @param navLanguage The user's navigator language.
+   */
+  getLanguage(navLanguage: string): string{
+    let languages: string[] = ["ar", "bg", "ca", "cs", "da", "de", "es", "fi", "fr", "hu", "id", "is", "it", "ja", "ko", "lt", "nl", "no", "pl", "pt-BR", "pt-PT", "ru", "sk", "sr", "sv", "th", "tr", "uk", "zh-CN", "zh-TW"];
+    for(let lang of languages){
+      if(navLanguage.includes(lang)){
+        return lang;
+      }
     }
-    else{
-      this.isDisabled = true;
+    return "en";
+  }
+
+  onSubmit(){
+    //Nothing to do here
+    console.log("submitting...");
+    
+    //Reset error in case we are trying again
+    this.isError = false;
+
+    if (this.contactForm.valid) 
+    {
+      //Create the object to send
+      let formResult = {
+        name: this.contactForm.value.name,
+        _replyto: this.contactForm.value.email,
+        message: this.contactForm.value.message,
+        _subject: "New submission!",
+        _language: this.language
+      }
+      console.log(formResult);
+
+      this.isSubmitted = true;
+
+      this.http.post('https://formspree.io/dbtext.contact@gmail.com', formResult)
+        .subscribe(
+          success => this.isSuccess = true,
+          error => 
+          { 
+            this.isError = true; 
+            console.error(error); 
+            this.isSubmitted = false;
+          }
+        );
     }
   }
 
