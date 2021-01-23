dfb707a59c4252ee9fcddacd3a4c3a1a75e844b3
detroitbecometext <keziabaj@gmail.com>
2021-01-23 17:35:13 +0100

    Typo fixes + Sidenav update

--- src/app/features/chloe/chloe/chloe.component.ts
+++ src/app/features/chloe/chloe/chloe.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+class Line {
+    content: string;
+    condition: string | null;
+}
+
 @Component({
-  selector: 'app-chloe',
-  templateUrl: './chloe.component.html',
-  styleUrls: ['./chloe.component.scss']
+    selector: 'app-chloe',
+    templateUrl: './chloe.component.html',
+    styleUrls: ['./chloe.component.scss'],
 })
 export class ChloeComponent implements OnInit {
+    public lines: Line[];
 
-  constructor() { }
+    constructor() {
+        this.lines = [];
+    }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
