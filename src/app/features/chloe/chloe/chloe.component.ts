160016f4590dd8edec01fa7db70b627743bd3d39
detroitbecometext <keziabaj@gmail.com>
2021-01-24 15:31:57 +0100

    Update Chloe component

--- src/app/features/chloe/chloe/chloe.component.ts
+++ src/app/features/chloe/chloe/chloe.component.ts
@@ -1,21 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-class Line {
-    content: string;
-    condition: string | null;
-}
-
 @Component({
     selector: 'app-chloe',
     templateUrl: './chloe.component.html',
     styleUrls: ['./chloe.component.scss'],
 })
 export class ChloeComponent implements OnInit {
-    public lines: Line[];
-
-    constructor() {
-        this.lines = [];
-    }
+    constructor() {}
 
     ngOnInit(): void {}
 }
