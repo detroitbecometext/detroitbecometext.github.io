85ad8fb621bb02b56e1e4d82abacf1d990eb3ac0
detroitbecometext <42693447+detroitbecometext@users.noreply.github.com>
2021-08-15 22:34:40 +0200

    Update chapters 4 and 5

--- src/app/features/chapters/chapter-contents/4-new-home-chapter-content/new-home-chapter-content.component.ts
+++ src/app/features/chapters/chapter-contents/4-new-home-chapter-content/new-home-chapter-content.component.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { TranslocoService } from '@ngneat/transloco';
 import { BaseTranslationComponent } from '@app/shared/base-translation/base-translation.component';
+import { RelationType } from '@app/core/models/relation-type.enum';
 
 @Component({
     selector: 'app-new-home-chapter-content',
@@ -8,6 +9,7 @@ import { BaseTranslationComponent } from '@app/shared/base-translation/base-tran
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NewHomeChapterContentComponent extends BaseTranslationComponent {
+    RelationType = RelationType;
     constructor(translocoService: TranslocoService) {
         super(translocoService);
     }
