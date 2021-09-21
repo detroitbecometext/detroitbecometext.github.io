8178fd20439e8eb5370d77571f65d67c05b3d473
detroitbecometext <42693447+detroitbecometext@users.noreply.github.com>
2021-09-21 19:52:40 +0200

    Add language keyboard navigation and fix observable issue

--- src/app/shared/base-translation/base-translation.component.ts
+++ src/app/shared/base-translation/base-translation.component.ts
@@ -1,3 +1,4 @@
+import { Component, HostListener } from '@angular/core';
 import { Character } from '@app/core/models/character.enum';
 import { RelationName } from '@app/core/models/relation-name.enum';
 import { RelationType } from '@app/core/models/relation-type.enum';
@@ -7,12 +8,22 @@ import { TranslocoService } from '@ngneat/transloco';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+interface ILanguage {
+    id: string;
+    label: string;
+}
+
+@Component({
+    template: '',
+})
 export abstract class BaseTranslationComponent {
     RelationType = RelationType;
     Character = Character;
     UnlockType = UnlockType;
     CommonTranslationKey = CommonTranslationKey;
     RelationName = RelationName;
+
+    private translatedObjects: Map<string, Observable<string>> = new Map();
 
     constructor(private readonly translocoService: TranslocoService) {}
 
@@ -22,16 +33,53 @@ export abstract class BaseTranslationComponent {
         endIndex: number | null = undefined,
         separator: string = ' '
     ): Observable<string> {
-        return this.translocoService
-            .selectTranslateObject(translationKey)
-            .pipe(
-                map((value) =>
-                    typeof value === 'string'
-                        ? value
-                        : Object.values(value)
-                              .slice(startIndex, endIndex)
-                              .join(separator)
-                )
+        if (!this.translatedObjects.has(translationKey)) {
+            this.translatedObjects.set(
+                translationKey,
+                this.translocoService
+                    .selectTranslateObject(translationKey)
+                    .pipe(
+                        map((value) =>
+                            typeof value === 'string'
+                                ? value
+                                : Object.values(value)
+                                      .slice(startIndex, endIndex)
+                                      .join(separator)
+                        )
+                    )
             );
+        }
+
+        return this.translatedObjects.get(translationKey);
+    }
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        let increment = 0;
+        if (event.key === 'l') {
+            increment = 1;
+        } else if (event.key === 'k') {
+            increment = -1;
+        } else {
+            return;
+        }
+
+        event.stopImmediatePropagation();
+
+        let currentLangId: string = this.translocoService.getActiveLang();
+        let allLangs: ILanguage[] =
+            this.translocoService.getAvailableLangs() as ILanguage[];
+        let currentLangIndex = allLangs.findIndex(
+            (l) => l.id === currentLangId
+        );
+        let nextIndex = currentLangIndex + increment;
+
+        if (nextIndex === allLangs.length) {
+            nextIndex = 0;
+        } else if (nextIndex < 0) {
+            nextIndex = allLangs.length - 1;
+        }
+
+        this.translocoService.setActiveLang(allLangs[nextIndex].id);
     }
 }
