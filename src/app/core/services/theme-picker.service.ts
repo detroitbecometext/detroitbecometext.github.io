b8d2337cf7295945bc6dd8d03677744b7789fd07
detroitbecometext <42693447+detroitbecometext@users.noreply.github.com>
2022-08-13 16:58:56 +0200

    Save selected theme in local storage

--- src/app/core/services/theme-picker.service.ts
+++ src/app/core/services/theme-picker.service.ts
@@ -5,23 +5,32 @@ import { BehaviorSubject, Observable } from 'rxjs';
     providedIn: 'root',
 })
 export class ThemePickerService {
+    private readonly storageKey = 'DBHT.THEME';
+
     private isDark: BehaviorSubject<boolean>;
     public isDark$: Observable<boolean>;
 
     constructor() {
-        let defaultValue: boolean = false;
-        if (
+        let isDark = false;
+
+        let storedValue: string | null = localStorage.getItem(this.storageKey);
+        if (storedValue !== null) {
+            isDark = JSON.parse(storedValue);
+        } else if (
             window.matchMedia &&
             window.matchMedia('(prefers-color-scheme: dark)').matches
         ) {
-            defaultValue = true;
+            isDark = true;
         }
 
-        this.isDark = new BehaviorSubject<boolean>(defaultValue);
+        this.isDark = new BehaviorSubject<boolean>(isDark);
         this.isDark$ = this.isDark.asObservable();
     }
 
     public switchTheme(): void {
-        this.isDark.next(!this.isDark.value);
+        let newValue = !this.isDark.value;
+
+        this.isDark.next(newValue);
+        localStorage.setItem(this.storageKey, JSON.stringify(newValue));
     }
 }
