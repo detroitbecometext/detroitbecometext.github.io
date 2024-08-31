fc8420a626b56f1f5cda625b7237c4e9c6abee18
detroitbecometext <42693447+detroitbecometext@users.noreply.github.com>
2024-08-31 11:49:38 +0200

    Add missing infos on case file

--- src/app/chapters/components/case-file/case-file.component.ts
+++ src/app/chapters/components/case-file/case-file.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { TranslocoModule } from '@jsverse/transloco';
 
+type SuspectValue = 'SUSPECT' | 'MISSING' | 'DESTROYED';
+
 @Component({
 	selector: 'app-case-file',
 	standalone: true,
@@ -12,4 +14,16 @@ import { TranslocoModule } from '@jsverse/transloco';
 })
 export class CaseFileComponent {
 	@Input({ required: true }) caseNumber: string = '';
+
+	public readonly caseSuspect: Map<string, SuspectValue> = new Map<
+		string,
+		SuspectValue
+	>([
+		['01', 'MISSING'],
+		['02', 'SUSPECT'],
+		['03', 'SUSPECT'],
+		['04', 'MISSING'],
+		['05', 'DESTROYED'],
+		['06', 'SUSPECT'],
+	]);
 }
